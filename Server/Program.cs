2a8aa2b3f20d8e0836d936237e56e6fc8b446146
Eddie Lin <a-eddielin@microsoft.com>
2023-02-21 00:03:05 +0800

    Add CorsPolicy

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,10 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("MyAllowAllHeadersPolicy",
+    options.AddPolicy("CorsPolicy",
         policy =>
         {
-            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            policy.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .SetIsOriginAllowed((host) => true)
+                  .AllowCredentials();
         });
 });
 
@@ -24,6 +27,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
