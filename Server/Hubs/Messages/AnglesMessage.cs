b3a3ed7ee51fc4d354492d6be3bdd06bec0a16fd
Eddie Lin <a-eddielin@microsoft.com>
2023-03-14 22:20:12 +0800

    fix id bug

--- Server/Hubs/Messages/AnglesMessage.cs
+++ Server/Hubs/Messages/AnglesMessage.cs
@@ -5,21 +5,21 @@ namespace Server.Hubs.Messages
     public class AnglesMessage
     {
         [JsonPropertyName("id")]
-        public string Id;
+        public string? Id { get; set; }
         [JsonPropertyName("timestamp")]
-        public string Timestamp;
+        public string? Timestamp { get; set; }
         [JsonPropertyName("ang1j")]
-        public double Ang1j;
+        public double? Ang1j { get; set; }
         [JsonPropertyName("ang2j")]
-        public double Ang2j;
+        public double? Ang2j { get; set; }
         [JsonPropertyName("ang3j")]
-        public double Ang3j;
+        public double? Ang3j { get; set; }
         [JsonPropertyName("ang4j")]
-        public double Ang4j;
+        public double? Ang4j { get; set; }
         [JsonPropertyName("ang5j")]
-        public double Ang5j;
+        public double? Ang5j { get; set; }
         [JsonPropertyName("ang6j")]
-        public double Ang6j;
+        public double? Ang6j { get; set; }
         public AnglesMessage(string id, string timestamp, double ang1j, double ang2j, double ang3j, double ang4j, double ang5j, double ang6j)
         {
             Id = id;
