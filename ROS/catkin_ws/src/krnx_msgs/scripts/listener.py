af52c0c1a3fe152c2d42d8624fda2a491972d3fb
Eddie Lin <a-eddielin@microsoft.com>
2023-03-14 19:01:00 +0800

    drop msg

--- ROS/catkin_ws/src/krnx_msgs/scripts/listener.py
+++ ROS/catkin_ws/src/krnx_msgs/scripts/listener.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 import sys
+import time
 import requests
 import json
 import rospy
@@ -36,7 +37,13 @@ class Watcher:
 
 
 class MyTopics(object):
-    def __init__(self, domain_name, hub):
+    def __init__(self, domain_name, hub, count):
+        self.count = count
+        self.count1 = 0
+        self.count2 = 0
+        self.count3 = 0
+        self.count4 = 0
+
         super(MyTopics, self).__init__()
         rospy.init_node("my_topics", anonymous=True)
 
@@ -97,53 +104,61 @@ class MyTopics(object):
     
     def sub_robot1_angles(self):
         def callback1(data1):
-            rospy.loginfo(f"robot1 receive: {data1}")
+            self.count1 = (self.count1 + 1) % self.count
+            if self.count1 == 0:
+                rospy.loginfo(f"robot1 receive: {data1}")
 
-            msg = self._build_message("SendAngles", [data1.id, data1.time, 
-                data1.joint_1, data1.joint_2, data1.joint_3, data1.joint_4, data1.joint_5, data1.joint_6])
-            self.signalr_ws.send(self.toSignalRMessage(msg))
-            rospy.loginfo(f"robot1 send: {msg}")
-            recv = self.signalr_ws.recv()
-            rospy.loginfo(f"receive signalr: {recv}")
+                msg = self._build_message("SendAngles", [data1.id, data1.time, 
+                    data1.joint_1, data1.joint_2, data1.joint_3, data1.joint_4, data1.joint_5, data1.joint_6])
+                self.signalr_ws.send(self.toSignalRMessage(msg))
+                rospy.loginfo(f"robot1 send: {msg}")
+                recv = self.signalr_ws.recv()
+                rospy.loginfo(f"receive signalr: {recv}")
         
         sub = rospy.Subscriber('robot1', RobotState, callback=callback1)
         rospy.spin()
                    
     def sub_robot2_angles(self):
         def callback2(data2):
-            rospy.loginfo(f"robot2 receive: {data2}")  
+            self.count2 = (self.count2 + 1) % self.count
+            if self.count2 == 0:
+                rospy.loginfo(f"robot2 receive: {data2}")  
 
-            msg = self._build_message("SendAngles", [data2.id, data2.time, 
-                data2.joint_1, data2.joint_2, data2.joint_3, data2.joint_4, data2.joint_5, data2.joint_6])
-            self.signalr_ws.send(self.toSignalRMessage(msg))
-            recv = self.signalr_ws.recv()
-            rospy.loginfo(f"receive signalr: {recv}")
+                msg = self._build_message("SendAngles", [data2.id, data2.time, 
+                    data2.joint_1, data2.joint_2, data2.joint_3, data2.joint_4, data2.joint_5, data2.joint_6])
+                self.signalr_ws.send(self.toSignalRMessage(msg))
+                recv = self.signalr_ws.recv()
+                rospy.loginfo(f"receive signalr: {recv}")
 
         sub = rospy.Subscriber('robot2', RobotState, callback=callback2)
         rospy.spin()
                    
     def sub_robot3_angles(self):
         def callback3(data3):
-            rospy.loginfo(f"robot3 receive: {data3}") 
+            self.count3 = (self.count3 + 1) % self.count
+            if self.count3 == 0:
+                rospy.loginfo(f"robot3 receive: {data3}") 
 
-            msg = self._build_message("SendAngles", [data3.id, data3.time, 
-                data3.joint_1, data3.joint_2, data3.joint_3, data3.joint_4, data3.joint_5, data3.joint_6])
-            self.signalr_ws.send(self.toSignalRMessage(msg))
-            recv = self.signalr_ws.recv()
-            rospy.loginfo(f"receive signalr: {recv}")
+                msg = self._build_message("SendAngles", [data3.id, data3.time, 
+                    data3.joint_1, data3.joint_2, data3.joint_3, data3.joint_4, data3.joint_5, data3.joint_6])
+                self.signalr_ws.send(self.toSignalRMessage(msg))
+                recv = self.signalr_ws.recv()
+                rospy.loginfo(f"receive signalr: {recv}")
 
         sub = rospy.Subscriber('robot3', RobotState, callback=callback3)
         rospy.spin()
                    
     def sub_robot4_angles(self):
         def callback4(data4):
-            rospy.loginfo(f"robot4 receive: {data4}") 
+            self.count4 = (self.count4 + 1) % self.count
+            if self.count4 == 0:
+                rospy.loginfo(f"robot4 receive: {data4}") 
 
-            msg = self._build_message("SendAngles", [data4.id, data4.time, 
-                data4.joint_1, data4.joint_2, data4.joint_3, data4.joint_4, data4.joint_5, data4.joint_6])
-            self.signalr_ws.send(self.toSignalRMessage(msg))
-            recv = self.signalr_ws.recv()
-            rospy.loginfo(f"receive signalr: {recv}")
+                msg = self._build_message("SendAngles", [data4.id, data4.time, 
+                    data4.joint_1, data4.joint_2, data4.joint_3, data4.joint_4, data4.joint_5, data4.joint_6])
+                self.signalr_ws.send(self.toSignalRMessage(msg))
+                recv = self.signalr_ws.recv()
+                rospy.loginfo(f"receive signalr: {recv}")
 
         sub = rospy.Subscriber('robot4', RobotState, callback=callback4)
         rospy.spin()
@@ -152,7 +167,8 @@ class MyTopics(object):
 if __name__ == "__main__":
     domain_name = os.getenv('DOMAIN_NAME', 'khi-signalr-server.azurewebsites.net')
     hub = os.getenv('HUB', 'robotic-arm-hub')
+    count = os.getenv('COUNT', '0')
     Watcher()
-    topics = MyTopics(domain_name, hub)
+    topics = MyTopics(domain_name, hub, int(count))
     topics.start()
     
