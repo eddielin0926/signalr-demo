a448d992278c84f45b2fb78b5ff336996ea54849
Eddie Lin <a-eddielin@microsoft.com>
2023-03-16 11:12:44 +0800

    add nyokkey webui

--- Server/wwwroot/js/nyokkey.js
+++ Server/wwwroot/js/nyokkey.js
@@ -7,5 +7,51 @@ connection.start().catch(function (err) {
 });
 
 connection.on("ReceiveLocation", function (message) {
-    console.log(message);
+    var t = document.getElementById('location');
+    t.innerHTML = `
+        <tr id="location">
+            <td>/nyokkey/hmi/location</td>
+            <td>${JSON.stringify(message)}</td>
+        </tr>
+    `;
+});
+
+connection.on("ReceiveRightArm", function (message) {
+    var t = document.getElementById('rightarm');
+    t.innerHTML = `
+        <tr id="rightarm">
+            <td>/nyokkey/hmi/rightarm</td>
+            <td>${JSON.stringify(message)}</td>
+        </tr>
+    `;
+});
+
+connection.on("ReceiveLeftArm", function (message) {
+    var t = document.getElementById('leftarm');
+    t.innerHTML = `
+        <tr id="leftarm">
+            <td>/nyokkey/hmi/leftarm</td>
+            <td>${JSON.stringify(message)}</td>
+        </tr>
+    `;
+});
+
+connection.on("ReceiveFace", function (message) {
+    var t = document.getElementById('face');
+    t.innerHTML = `
+        <tr id="face">
+            <td>/nyokkey/hmi/face</td>
+            <td>${JSON.stringify(message)}</td>
+        </tr>
+    `;
+});
+
+connection.on("ReceiveTask", function (message) {
+    var t = document.getElementById('task');
+    t.innerHTML = `
+        <tr id="task">
+            <td>/nyokkey/hmi/task</td>
+            <td>${JSON.stringify(message)}</td>
+        </tr>
+    `;
 });
--- Server/wwwroot/js/roboticArm.js
+++ Server/wwwroot/js/roboticArm.js
@@ -2,9 +2,6 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/robotic-arm-hub").build();
 
-//Disable the send button until connection is established.
-document.getElementById("sendButton").disabled = true;
-
 connection.on("ReceiveMessage", function (message) {
     console.log(message);
     var t = document.getElementById(message.id);
@@ -22,20 +19,6 @@ connection.on("ReceiveMessage", function (message) {
     `;
 });
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
+connection.start().catch(function (err) {
     return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var str = document.getElementById("msgInput").value;
-    var data = str.split(",");
-    var angs = data.slice(-6).map((v) => parseFloat(v));
-    if (data) {
-        connection.invoke("SendAngles", data[0], data[1], ...angs).catch(function (err) {
-            return console.error(err.toString());
-        });
-    }
-    event.preventDefault();
 });
